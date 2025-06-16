517c2f2336d823972d83a0f723431c5c852e53a7
anower77 <anowerhossain765562@gmail.com>
2025-06-16 21:15:05 +0600

    deploy V.0.0.5

--- CP_Platfrom/urls.py
+++ CP_Platfrom/urls.py
@@ -2,23 +2,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 # Swagger imports
 from .views import schema_view
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({
+        "status": "success",
+        "message": "CP Platform API is live"
+    })),
+
+    # Admin and API routes
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
-    # # path('', include("home_feed.urls")),
     path('problem_list/', include("problem_list.urls")),
     path('dashboard/', include('dashboard.urls')),
-    # path('contest/', include("contest.urls")),
-    # path('topics/', include('topics.urls')),
 
     # Swagger Documentation
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
 ]
 
+# Serve media files during development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
